73ffffc5a09aa738d134051b7e90c8c018a88fdc
Loris Leiva <loris.leiva@gmail.com>
2025-12-19 18:25:46 +0000

    Add Airdrop plugin (#4)
    See [plugin documentation](https://github.com/anza-xyz/kit-plugins/blob/bdd61c4c05b3943cc653458d6284b789a406ae84/packages/kit-plugin-airdrop/README.md).

---
+++ packages/kit-plugin-airdrop/src/index.ts
@@ -0,0 +1,73 @@
+import { Address, airdropFactory, Lamports } from '@solana/kit';
+
+type LiteSVMClient = {
+    svm: { airdrop: (address: Address, lamports: Lamports) => unknown };
+};
+type RpcClient = {
+    rpc: Parameters<typeof airdropFactory>[0]['rpc'];
+    rpcSubscriptions: Parameters<typeof airdropFactory>[0]['rpcSubscriptions'];
+};
+
+/**
+ * Function type for the `airdrop` method added to the client.
+ *
+ * @param address - The address to which the airdrop will be sent.
+ * @param amount - The amount of lamports to airdrop.
+ */
+export type AirdropFunction = (address: Address, amount: Lamports) => Promise<void>;
+
+/**
+ * A plugin that adds an `airdrop` method to the client.
+ *
+ * This will use the LiteSVM's internal airdrop method if the
+ * client has LiteSVM installed. Otherwise, it will rely on the
+ * `Rpc` and `RpcSubscriptions` clients to perform the airdrop.
+ *
+ * @example
+ * RPC-based airdrop.
+ * ```ts
+ * import { createEmptyClient } from '@solana/kit';
+ * import { airdrop, localhostRpc } from '@solana/kit-plugins';
+ *
+ * // Install the airdrop plugin using a localhost RPC.
+ * const client = createEmptyClient()
+ *     .use(localhostRpc())
+ *     .use(airdrop());
+ *
+ * // Use the airdrop method.
+ * client.airdrop(myAddress, lamports(1_000_000_000n));
+ * ```
+ *
+ * @example
+ * LiteSVM-based airdrop.
+ * ```ts
+ * import { createEmptyClient } from '@solana/kit';
+ * import { airdrop, litesvm } from '@solana/kit-plugins';
+ *
+ * // Install the airdrop plugin using a LiteSVM instance.
+ * const client = createEmptyClient()
+ *     .use(litesvm())
+ *     .use(airdrop());
+ *
+ * // Use the airdrop method.
+ * client.airdrop(myAddress, lamports(1_000_000_000n));
+ * ```
+ *
+ * @see {@link AirdropFunction}
+ */
+export function airdrop() {
+    return <T extends LiteSVMClient | RpcClient>(client: T) => {
+        if ('svm' in client) {
+            const airdrop: AirdropFunction = (address, amount) => {
+                client.svm.airdrop(address, amount);
+                return Promise.resolve();
+            };
+            return { ...client, airdrop };
+        }
+        const airdropInternal = airdropFactory(client);
+        const airdrop: AirdropFunction = async (address, amount) => {
+            await airdropInternal({ commitment: 'confirmed', lamports: amount, recipientAddress: address });
+        };
+        return { ...client, airdrop };
+    };
+}
---
+++ packages/kit-plugin-airdrop/test/index.test.ts
@@ -0,0 +1,60 @@
+import { address, lamports, mainnet } from '@solana/kit';
+import { litesvm } from '@solana/kit-plugin-litesvm';
+import { localhostRpc, rpc } from '@solana/kit-plugin-rpc';
+import { createEmptyClient } from '@solana/plugin-core';
+import { describe, expect, it, vi } from 'vitest';
+
+import { airdrop } from '../src';
+
+describe('airdrop', () => {
+    it('provides an airdrop function that relies on RPCs', () => {
+        const getSignatureStatuses = vi.fn();
+        const requestAirdrop = vi.fn();
+        const signatureNotifications = vi.fn();
+        const client = createEmptyClient()
+            .use(() => ({
+                rpc: { getSignatureStatuses, requestAirdrop },
+                rpcSubscriptions: { signatureNotifications },
+            }))
+            .use(airdrop());
+        expect(client).toHaveProperty('airdrop');
+        expect(typeof client.airdrop).toBe('function');
+    });
+
+    it('provides an airdrop function that relies on a LiteSVM instance', async () => {
+        const svm = { airdrop: vi.fn() };
+        const client = createEmptyClient()
+            .use(() => ({ svm }))
+            .use(airdrop());
+        expect(client).toHaveProperty('airdrop');
+        expect(typeof client.airdrop).toBe('function');
+
+        const receiver = address('HQVxiMVDoV9jzG4tpoxmDZsNfWvaHXm8DGGv93Gka75v');
+        const amount = lamports(1_000_000_000n);
+        await client.airdrop(receiver, amount);
+        expect(svm.airdrop).toHaveBeenCalledWith(receiver, amount);
+    });
+
+    it('works with an arbitrary RPC', () => {
+        const client = createEmptyClient().use(rpc('https://my-rpc.com')).use(airdrop());
+        expect(client).toHaveProperty('airdrop');
+    });
+
+    it('works with a localhost RPC', () => {
+        const client = createEmptyClient().use(localhostRpc()).use(airdrop());
+        expect(client).toHaveProperty('airdrop');
+    });
+
+    it('throws a TypeScript error with a mainnet RPC', () => {
+        const client = createEmptyClient()
+            .use(rpc(mainnet('https://my-rpc.com')))
+            // @ts-expect-error Airdrop RPC methods are not available on mainnet.
+            .use(airdrop());
+        expect(client).toHaveProperty('airdrop');
+    });
+
+    it('works with a LiteSVM instance', () => {
+        const client = createEmptyClient().use(litesvm()).use(airdrop());
+        expect(client).toHaveProperty('airdrop');
+    });
+});
